c283fa9cc96a1f23c6a0f5d55d4b4b32a412d20a
Dmitry <tkadim@gmail.com>
2021-11-28 21:52:41 +0200

    Swift home tasks

---
+++ HomeTask1.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+//let daysPerMonth = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+
+//for i in 0...11 {
+//    print("In \(months[11-i].0) \(months[11-i].1) days")
+//}
+
+//func getNumberOfDays(month: String, day: Int) -> Int {
+//
+//    var days = 0
+//
+//    let months = [("Jan",31),("Feb",28),("Mar",31),("Apr",30),("May",31),("Jun",30),("Jul",31),("Aug",31),("Sep",30),("Oct",31),("Nov",30),("Dec",31)]
+//
+//    for i in 0...months.count - 1 {
+//        if months[i].0 == month {
+//            for j in 0...i {
+//                days += months[j].1
+//            }
+//            days -= months[i].1 - day
+//        }
+//    }
+//
+//    return days
+//}
+//
+//print("There are \(getNumberOfDays(month: "Feb", day: 16)) days from beginning of the year")
+
+//====================================================================
+
+
